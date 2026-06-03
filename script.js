d1efad6efeefbbf9015b733b4e784ace3e83b029
k rahul pandit <147794873+rahulpandit-123@users.noreply.github.com>
2026-06-03 14:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+	get species(){
+		 return this.species=speices;
+	}
 
-class Dog extends Animal {}
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`)
+	}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof")
+	}
+}
 
+class Cat extends Animal {
+	purr(){
+		console.log("purr")
+	}
+}
+const myCat = new Cat("Siamese");
+myCat.makeSound(); 
+myCat.purr();      
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); 
+myDog.bark();  
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
